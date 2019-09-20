25e9960958c4354b90fa14d74d6c2578ab8d4799
surajg1 <surajgholap1234@gmail.com>
2019-09-20 20:46:46 +0530

    app title changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
 
+import { Todo } from "./todo";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+  title = 'Suraj ToDo App.';
+
+  todoValue:string;
+  list :Todo[];
+
+  ngOnInit(){
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.list=[];
+    this.todoValue="";
+  }
+
+  addItem(){
+    if(this.todoValue !== ""){
+
+      const newItem : Todo ={
+        id: Date.now(),
+        values:this.todoValue,
+        isDone: false
+      };
+      this.list.push(newItem);
+    }
+    this.todoValue= "";
+  }
+
+  deleteItem(id:number){
+    this.list=this.list.filter(item => item.id!==id);
+    
+  }
 }
